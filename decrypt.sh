c8debdbfccb0f5f44b9c8aa84462a9e0f632b8a4
root <root@bayview>
2024-09-18 02:28:15 +0000

    changes from bayview on Wed Sep 18 02:28:15 UTC 2024

--- decrypt.sh
+++ decrypt.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
+# ref https://www.linkedin.com/pulse/day-2-learn-bash-write-script-encrypt-filefolder-hasan-mahmud-
 echo "enter the password"
 read -s password
 
-openssl enc -aes-256-cbc -d -in ~/mycode/repos/my_random_scripts .env.enc -out ~/mycode/.env -k $password
-
-echo "File decrypted successfully"
+openssl enc -aes-256-cbc -d -in ~/mycode/repos/My_Random_Scripts/.env.enc -out ~/mycode/.env -k $password
+	if [ -e ~/mycode/.env ]; then
+		printf ".env file installed successfully\n"
+	else
+		printf " .env file failed to decrypted successfully\n"
+	fi
+. ~/.bashrc >/dev/null 2>&1	# runs script silently
+printf "${RED}this should be in red to prove .env is working${NORMAL}\n"
